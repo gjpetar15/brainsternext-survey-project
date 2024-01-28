1352b3292762fc16562f57de1c22f2e61c096f8f
Petar Gjorgjioski <gjorgjioski.a22011@next.edu.mk>
2024-01-28 17:58:36 +0100

    save survey on button

--- src/app/dashboard/surveys/create/page.tsx
+++ src/app/dashboard/surveys/create/page.tsx
@@ -1,7 +1,32 @@
-"use client";
-
 import SurveyForm from "@/components/SurveyForm/SurveyForm";
+import { SurveyDTO } from "@/types/SurveyDTO";
+import { SurveyStatus } from "@prisma/client";
 
 export default function SurveyCreatePage() {
-  return <SurveyForm title="Create a Survey" />;
+  const createSurvey = async (formData: FormData) => {
+  "use server";
+  const data: Partial<SurveyDTO["data"]> = {
+    name: formData.get("name") as string,
+    manager: formData.get("manager") as string,
+    introduction: formData.get("introduction") as string,
+    status: formData.get("status") as SurveyStatus,
+  };
+
+  try {
+    const response = await fetch(`${process.env.API_URL}/surveys`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData.data.id;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+  return <SurveyForm 
+        title="Create a Survey" 
+        updateSurvey={createSurvey}
+  />;
 }
